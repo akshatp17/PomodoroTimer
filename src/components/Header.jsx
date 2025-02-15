405f3b9a3997be8c3f701ccfa8ef577bfc0da7cd
Akshat Pratyush <akshatp0505@gmail.com>
2025-02-15 18:32:16 +0530

    added header and timer

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,10 +4,10 @@ import { motion } from 'framer-motion'
 const Header = () => {
     return (
         <div className='flex flex-col justify-center items-center bg-sky-100'>
-            <div className=''>
+            <div className='text-6xl font-bold'>
                 Pomotive
             </div>
-            <div className=''>
+            <div className='text-xl'>
                 Pomodoro Timer for Productivity
             </div>
         </div>
--- src/components/Timer.jsx
+++ src/components/Timer.jsx
@@ -4,40 +4,60 @@ import { motion } from 'framer-motion'
 
 const Timer = () => {
 
-    const [minutes, setMinutes] = useState(25)
-    const [seconds, setSeconds] = useState(0)
+    const [minutes, setMinutes] = useState(0)
+    const [seconds, setSeconds] = useState(3)
     const [timer, setTimer] = useState(false)
 
     useEffect(() => {
-        if (timer) {
-            let interval = setInterval(() => {
-                clearInterval(interval);
+        if (!timer) return;
 
-                if (seconds === 0) {
-                    if (minutes !== 0) {
-                        setSeconds(59);
-                        setMinutes(minutes - 1);
-                    } else {
-                        setMinutes(25);
-                        setSeconds(0);
-                        setTimer(false)
-                    }
-                } else {
-                    setSeconds(seconds - 1);
+        const interval = setInterval(() => {
+            setSeconds((prevSeconds) => {
+                if (prevSeconds === 0) {
+                    setMinutes((prevMinutes) => {
+                        if (prevMinutes === 0) {
+                            clearInterval(interval);
+                            setTimer(false);
+
+                            return 0;
+                        }
+                        return prevMinutes - 1;
+                    });
+                        return 59;
                 }
-            }, 1000);
-        }
-    }, [seconds]);
+                return prevSeconds - 1;
+            });
+        }, 1000);
+
+        return () => clearInterval(interval);
+    }, [timer]);
 
     const timerMinutes = minutes < 10 ? `0${minutes}` : minutes;
     const timerSeconds = seconds < 10 ? `0${seconds}` : seconds;
 
+    const toggleStartPause = () => {
+        setTimer(!timer)
+    }
+
+    const toggleReset = () => {
+        setTimer(false)
+        setMinutes(25)
+        setSeconds(0)
+    }
+
     return (
         <>
             <div className='flex flex-col justify-center items-center w-[100%]'>
-                <div className='timerContent text-center'>
+                <div>
+                    <button>Break</button>
+                    <button>Pomodoro</button>
+                    <button>Long Break</button>
+                </div>
+                <div className='timerContent text-center text-6xl font-bold'>
                     {timerMinutes}:{timerSeconds}
                 </div>
+                <button onClick={toggleStartPause} className='hover:cursor-pointer'>Start</button>
+                <button onClick={toggleReset} className='hover:cursor-pointer'>Reset</button>
             </div>
         </>
     )
